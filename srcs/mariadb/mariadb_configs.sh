2ff64bc427f1ff345a718fea6fc3f7032d82206c
dspereira <diogo.pereira.ee@gmail.com>
2023-10-27 17:34:45 +0100

    Fixed a bug in mariadb_configs.sh where the query for 'FLUSH PRIVILEGES' was producing an error.

--- srcs/mariadb/mariadb_configs.sh
+++ srcs/mariadb/mariadb_configs.sh
@@ -4,12 +4,17 @@ if [ ! -d "/var/lib/mysql/my_db" ]; then
 
     /etc/init.d/mariadb start
 
+    sleep 1
+
     mysql -u root -e "CREATE DATABASE my_db;"
     mysql -u root -e "CREATE USER 'dsilveri'@'%' IDENTIFIED BY '123';"
     mysql -u root -e "GRANT ALL PRIVILEGES ON *. * TO 'dsilveri'@'%';"
-    mysql -u root -e FLUSH PRIVILEGES;
+    mysql -u root -e "FLUSH PRIVILEGES;"
 
     /etc/init.d/mariadb stop
+
+else
+    echo "Database is already configured"
 
 fi
 
